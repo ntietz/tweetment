43902948921e1c4c69fcadb2ff8cccd5ee520727
ntietz <ntietz@gmail.com>
2015-11-13 15:21:32 -0500

    Added main (forgot to git add it a while ago) and gitignore.

---
+++ main.py
@@ -0,0 +1,19 @@
+import argparse
+import sentiment
+
+parser = argparse.ArgumentParser(description='Runs the response generator code.')
+parser.add_argument('--train-sentiment', action='store_true', help='Run the tweet sentiment classifier trainer')
+args, uknowns = parser.parse_known_args()
+
+if args.train_sentiment:
+  print 'Training sentiment classifier...'
+
+  # Reparse with the arguments we want now
+  sentiment.add_arguments(parser)
+  args = parser.parse_args()
+  print args
+
+  sentiment.main(args)
+else:
+  print 'No options matched.'
+
