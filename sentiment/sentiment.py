ce6de108d77e88f194fb21eb4ea981e4e91a87c8
Nicole Tietz-Sokolskaya <me@ntietz.com>
2015-11-13 14:55:39 -0500

    Added punctuation and clusters features

--- sentiment/sentiment.py
+++ sentiment/sentiment.py
@@ -1,6 +1,8 @@
+import argparse
 import sklearn
 import string
 import tweetmotif
+
 
 def corpus_ngrams(corpus):
   '''
@@ -21,7 +23,8 @@ def corpus_ngrams(corpus):
   for length in char_lengths:
     char_ngrams[length] = set()
 
-  for tweet in corpus:
+  for record in corpus:
+    tweet, _, _, _ = record
     for length in char_lengths:
       for idx in range(0, len(tweet) - length + 1):
         char_ngrams[length].add(tweet[idx : idx+length])
@@ -38,11 +41,49 @@ def corpus_ngrams(corpus):
   #print "words: %s \n\n nonc: %s \n\n char: %s \n\n" % (word_ngrams, nonc_ngrams, char_ngrams)
   return word_ngrams, nonc_ngrams, char_ngrams
 
+
 def word_is_all_caps(word):
   for c in word:
     if c not in string.ascii_uppercase:
       return False
   return True
+
+
+def contains_question_or_exclaim(token):
+  for c in token:
+    if c == '?' or c == '!':
+      return True
+  return False
+
+
+def num_contiguous_question_exclaim(tweet):
+  num_q = 0
+  num_e = 0
+  num_qe = 0
+  entered_seq = False
+  mixed = False
+  last_was = ''
+  for c in tweet + ' ': # add an extra space for an extra iteration (hack)
+    if entered_seq:
+      if c != '!' and c != '?':
+        # End of sequence
+        if mixed:
+          num_qe += 1
+        elif last_was == '!':
+          num_e += 1
+        else:
+          num_q += 1
+        entered_seq = False
+        mixed = False
+      elif c != last_was:
+        # Mixture of ! and ? must be here
+        mixed = True
+    else:
+      if c == '!' or c == '?':
+        entered_seq = True
+    last_was = c
+  return num_q, num_e, num_qe
+
 
 def is_elongated(word):
   elong_len = 3
@@ -51,6 +92,7 @@ def is_elongated(word):
       return True
   return False
 
+
 def contains_emoticon(s):
   '''
     Checks if the passed-in string contains any emoticons.
@@ -58,7 +100,33 @@ def contains_emoticon(s):
   '''
   pass # TODO
 
-def generate_features(tweet):
+
+def load_clusters(cluster_filename):
+  clusters_to_words = {}
+  words_to_clusters = {}
+  clusters = {}
+  idx = 0
+  with open(cluster_filename) as f:
+    for line in f:
+      (cluster, word, count) = line.split()
+      if not cluster in clusters_to_words:
+        clusters_to_words[cluster] = set()
+        clusters[cluster] = idx
+        idx += 1
+      clusters_to_words[cluster].add(word)
+      words_to_clusters[word] = cluster
+  return words_to_clusters, clusters_to_words, clusters
+
+
+def get_cluster_mem_vec(cids, w2c, words):
+  vec = [0]*len(cids)
+  for word in words:
+    if word in w2c:
+      vec[cids[w2c[word]]] = 1
+  return vec
+
+
+def generate_features(tweet, w2c, cids):
   '''
     Takes in a tweet and generates a feature vector.
   '''
@@ -72,10 +140,10 @@ def generate_features(tweet):
     - pos
     + hashtags
     - lexicons
-    - punctuation
+    + punctuation
     / emoticons
     + elongated words
-    - clusters
+    + clusters
     - negation
   '''
 
@@ -90,8 +158,29 @@ def generate_features(tweet):
     if is_elongated(word):
       num_elongated += 1
 
-def main():
-  corpus = ["This is a tweet looong #happy #sad .", "This is 'nt a status message ."]
-  word_ngrams, nonc_ngrams, char_ngrams = corpus_ngrams(corpus)
-  print generate_features(corpus[0])
+  last_is_question_or_exclaim = 1 if contains_question_or_exclaim(words[-1]) else 0
+  num_seq_question, num_seq_exclaim, num_seq_both = num_contiguous_question_exclaim(tweet)
+  cluster_mem_vec = get_cluster_mem_vec(cids, w2c, words)
+
+  features = [num_allcaps, num_hashtags, num_elongated, last_is_question_or_exclaim, num_seq_question, num_seq_exclaim, num_seq_both] + cluster_mem_vec
+  return features
+
+
+def add_arguments(parser):
+  parser.add_argument('--input', type=str, required=True, help='Input file, which must be the output from Twokenize.')
+  parser.add_argument('--clusters', type=str, required=True, help='The file containing the clusters data.')
+
+
+def main(args):
+  with open(args.input) as f:
+    corpus = []
+    for line in f:
+      tok_tweet, pos, pos_conf, orig_tweet = line.strip().split('\t')
+      corpus.append((tok_tweet, pos, pos_conf, orig_tweet))
+    w2c, c2w, cids = load_clusters(args.clusters)
+    word_ngrams, nonc_ngrams, char_ngrams = corpus_ngrams(corpus)
+    for record in corpus:
+      tweet, _, _, _ = record
+      generate_features(tweet, w2c, cids)
+    print generate_features(corpus[1][0], w2c, cids)
 
